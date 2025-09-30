35d26c9ddd39b45383c64f3e4369c5d14385c9fb
Shwetank Raj <yo.shwetankraj@gmail.com>
2025-09-30 19:16:15 +0530

    Added routing setup with Navbar and Login Page skeleton

---
+++ src/pages/Login.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="text-center mt-5">
+      <h2>Login Page</h2>
+      <p>OTP login functionality will be implemented here.</p>
+    </div>
+  );
+}
